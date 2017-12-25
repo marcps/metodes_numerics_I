7a723e3a922c1d48e4b16b8ca28da7229d7a4c66
marcps <marc.ps.12@gmail.com>
2017-12-25 20:21:10 +0100

    ex1_3.c update

--- Practiques/3_interp_polinom/ex1_3.c
+++ Practiques/3_interp_polinom/ex1_3.c
@@ -1,8 +1,12 @@
-/*EXERCICI 1: Marc Pascual i Solé - Universitat de Barcelona 
-Programa que troba els coeficients del polinomi interpolador p(x)=a0+...+an·x^n
-Fent-ho de forma matricial X·a=y
-On X és la matriu de punts abcisses de totes les potencies (0%n), y és el vector 
-de punts ordenades de suport, i a és el vector de coeficients que volem trobar.*/
+/*################################################################################
+#------------------------------------------------------------------------------  #
+#EXERCICI 1 - Interpolació: Marc Pascual i Solé - Universitat de Barcelona    	 #
+#--------------------------------------------------------------------------------#
+#Programa que troba els coeficients del polinomi interpolador p(x)=a0+...+an·x^n #
+#Fent-ho de forma matricial X·a=y												 #
+#On X és la matriu de punts abcisses de totes les potencies (0%n), y és el vector# 
+#de punts ordenades de suport, i a és el vector de coeficients que volem trobar. #
+#################################################################################*/
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
@@ -52,17 +56,30 @@ void backwards_subs(int n, double *x,double **A)
     }  
 }
 
-matrix_init(int n, int m, double**A)
+void matrix_init(int n, int m, double **A)
 {
   int i;
   //n=row number, m=column number
   A=(double **)malloc(n*sizeof(double));
-  if (A==NULL) fprintf(stderr, "Error de memoria");
+  if (A==NULL) 
+  	{
+  		fprintf(stderr, "Error de memoria");
+  	}
   for (i=0;i<n;i++)
     {
       A[i]=(double *)malloc(m*sizeof(double));
-      if (A[i]==NULL) fprintf(stderr, "Error de memoria");
+      if (A[i]==NULL) {
+      	fprintf(stderr, "Error de memoria");
+      }
     }
+}
+
+void vector_init(int n, double *x)
+{
+  int i;
+  //n=row number
+  x=(double *)malloc(n*sizeof(double));
+  if (x==NULL) fprintf(stderr, "Error de memoria");
 }
 
 
@@ -70,38 +87,58 @@ matrix_init(int n, int m, double**A)
 int main()
 {
   int n,i,j,k;
-  double temp, **A;
+  double tempu, **A, *a;
 
-  printf("Nombre n de passes [n]: ");
-  scanf("%d",n);
+  printf("Grau del polinomi[n]: ");
+  scanf("%d",&n);
 
   //S'inicialitza la matriu A (n+1)x(n+2)
-  matrix_init(n+1,n+2,A);
+  A=(double **)malloc((n+1)*sizeof(double));
+  if (A==NULL) fprintf(stderr, "Error de memoria");
+  for (i=0;i<n+1;i++)
+    {
+      A[i]=(double *)malloc((n+2)*sizeof(double));
+      if (A[i]==NULL)fprintf(stderr, "Error de memoria");
+    }
+   //S'inicialitza el vector incògnita
+   a=(double *)malloc((n+1)*sizeof(double));
 
   //-------------- es llegeixen les dades:---------------------------------
 
   for (i=0;i<=n;i++) //n+1 vegades
     {
-      printf("x[%d]=",i);
-      scanf("%lf",temp);
+      printf("x[%d]=",i+1);
+      scanf("%lf", &tempu);
       for (j=0;j<=n;j++)
 		{
-	  	  A[i][j]=pow(temp,j);
+	  	  A[i][j]=pow(tempu,j);
 	  	  //La primera columna seran tot 1's
 		}
     }
 
-  printf("Vector f:\n\n");
-  for (i=0;i<n;i++)
+  printf("\nVector f:\n\n");
+  for (i=0;i<n+1;i++)
     {
       printf("f[%d]",i+1);
-      scanf("%lf",A[i][n+1]); //Es llegeixen els valors de la última columna de la matriu ampliada
+      scanf("%lf",&A[i][n+1]); //Es llegeixen els valors de la última columna de la matriu ampliada
     }
   // --------------------------------------------------------------------------
 
   //---------------- S'aplica eliminació Gaussiana ----------------------------
+  gaussian_elim(n+1, A);
+  backwards_subs(n+1, a,A);
 
+  //S'imprimeixen els resultats:
+  printf("\n\n RESULTATS:\n");
+  for (i=0;i<=n;i++)
+    {
+      printf("a_%d=%lf\n",i,a[i]);
+    }
 
-  //
+  free(A);
+  free(a);
+
+  return 0;
+}
 
      
